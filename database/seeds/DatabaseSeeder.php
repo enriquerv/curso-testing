975b30485d361f156a9f797a0a0be95bcdc050ef
Enrique Rodriguez <erodriguez@fabricadesoluciones.com>
2024-10-13 02:59:43 -0600

    primer commit

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -15,5 +15,6 @@ class DatabaseSeeder extends Seeder
         $this->call(StatesTableSeeder::class);
         $this->call(CitiesTableSeeder::class);       
         $this->call(UsersTableSeeder::class);
+        $this->call(ProductsSeeder::class);
     }
 }
